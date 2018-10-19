129dd58bd6abda45d84b59733d5d22586feece1d
shamisonn <unknown0incident@gmail.com>
2018-10-19 12:22:53 +0900

    fix lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, {render} from 'react-dom';
+import { render } from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
