73c787aba0a5b8dc29dcd983113b3ae159dbe12c
shamisonn <unknown0incident@gmail.com>
2018-10-19 14:21:36 +0900

    ダサすぎたのでちょっと整えてみた

--- workbox-config.js
+++ workbox-config.js
@@ -4,5 +4,17 @@ module.exports = {
 	"**/*.{json,ico,png,html,js,css}"
     ],
     "swDest": "build/sw.js",
-    "navigateFallback": '/index.html'
+    "navigateFallback": '/index.html',
+    "runtimeCaching": [
+	{
+	    urlPattern: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO",
+	    options: {
+		cacheName: 'bootstrap-min',
+		expiration: {
+		    maxEntries: 80,
+		    maxAgeSeconds: 60 * 60 * 24
+		}
+	    }
+	}
+    ]
 };
